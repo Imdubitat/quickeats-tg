311c69fe096644defe20a1c945382336e967aa9b
lais <laismtelles@gmail.com>
2025-02-11 10:19:23 -0300

    Modal de cadastro

--- quickeats/resources/views/index_cliente.blade.php
+++ quickeats/resources/views/index_cliente.blade.php
@@ -5,11 +5,11 @@
 @section('nav-buttons')
     <ul class="nav d-flex flex-wrap justify-content-start">
         <li class="nav-item">
-            <a href="" id="cliente" class="btn btn-custom ms-4" data-bs-toggle="modal" data-bs-target="#signinModal">Tenho conta</a>
+            <a href="" id="login" class="btn btn-custom ms-4" data-bs-toggle="modal" data-bs-target="#signinModal">Tenho conta</a>
         </li>
 
         <li class="nav-item">
-            <a href="" id="restaurante" class="btn btn-custom2 ms-4">Cadastrar</a>
+            <a href="" id="signup" class="btn btn-custom2 ms-4" data-bs-toggle="modal" data-bs-target="#signupModal">Cadastrar</a>
         </li>
     </ul>
 @endsection
@@ -38,10 +38,57 @@
                         <label for="senhaLogin">Senha</label>
                     </div>
                     <div class="d-flex justify-content-center">
-                        <p>Ainda não tem uma conta? <a href="#">Cadastre-se</a></p>
+                        <p>Ainda não tem uma conta? <a href="#" data-bs-toggle="modal" data-bs-target="#signupModal">Cadastre-se</a></p>
                     </div>
                     <div class="d-flex justify-content-center">
                         <button type="submit" class="btn btn-custom4 w-50">Entrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal de cadastro -->
+<div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document" style="max-width: 350px;">
+        <div class="modal-content rounded-4">
+            <div class="modal-header">
+                <h5 class="modal-title fw-bold" id="signupModalLabel">Cadastro</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    @csrf
+                    <div class="form-floating mb-3">
+                        <input id="nomeSignup" type="text" class="form-control rounded-4" placeholder="Nome">
+                        <label for="nomeSignup">Nome Completo</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input id="cpfSignup" type="text" class="form-control rounded-4" placeholder="CPF">
+                        <label for="cpfSignup">CPF</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input id="dataNascSignup" type="date" class="form-control rounded-4" placeholder="Data de nascimento">
+                        <label for="dataNascSignup">Data de nascimento</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input id="telefoneSignup" type="text" class="form-control rounded-4" placeholder="Telefone">
+                        <label for="telefoneSignup">Telefone</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input id="emailSignup" type="email" class="form-control rounded-4" placeholder="Email">
+                        <label for="emailSignup">Email</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input id="senhaSignup" type="password" class="form-control rounded-4" placeholder="Senha">
+                        <label for="senhaSignup">Senha</label>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <p>Já tem uma conta? <a href="#" data-bs-toggle="modal" data-bs-target="#signinModal">Faça Login</a></p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn btn-custom4 w-50">Cadastrar</button>
                     </div>
                 </form>
             </div>
