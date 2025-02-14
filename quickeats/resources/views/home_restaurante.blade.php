6ef6beffe535f7b7114261d3eb51b002490a9293
lais <laismtelles@gmail.com>
2025-02-14 17:25:20 -0300

    Adição 'template_restaurante'

--- quickeats/resources/views/home_restaurante.blade.php
+++ quickeats/resources/views/home_restaurante.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+@extends('template_restaurante')
+
+@section('title', 'Home | Restaurante')
+
+@section('nav-buttons')
+
+@endsection
