6ee5da6265c9a339080bc58f517be30be4c6cdad
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2025-02-16 20:54:40 -0300

    Página com catálogo de produtos cadastrados no banco de dados

--- quickeats/resources/views/catalogo_produtos.blade.php
+++ quickeats/resources/views/catalogo_produtos.blade.php
@@ -1,6 +1,6 @@
 @extends('template_cliente')
 
-@section('title', 'Home | Cliente')
+@section('title', 'Catálogo de produtos')
 
 @section('nav-buttons')
 
@@ -8,18 +8,29 @@
 
 @section('content')
 <section class="px-5" style="margin-top: 15rem;">
-    <div class="row mx-5">
+    @if(session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+    <div class="row mx-auto">
         @foreach($produtos as $p)
             <div class="col-md-4 mb-4">
-                <form action="" method="GET">
+                <form action="{{ route ('adicionar_carrinho') }}" method="POST">
                     @csrf
                     <input type="hidden" name="produto" value="{{ $p->id_produto }}">
+                    <input type="hidden" name="data_adicao" value="{{ now() }}">
                     <div class="card shadow rounded-4">
                         <div class="card-body">
                             <h5 class="card-title">{{ $p->nome_produto }}</h5>
                             <p class="card-text">R$ {{ $p->valor }}<br>
                             {{ $p->estab }}</p>
-                            <a type="submit" href="" class="btn btn-custom3">Adicionar ao carrinho</a>
+                            <label for="qtd_produto_{{ $p->id_produto }}" class="form-label">Quantidade:</label>
+                            <input type="text" name="qtd_produto" id="qtd_produto_{{ $p->id_produto }}"
+                            class="form-control-sm mb-2" value="1" min="1" style="width: 30px; height: 10px;" required><br>
+                            
+                           <button type="submit" class="btn btn-custom3">Adicionar ao carrinho</button>
                         </div>
                     </div>
                 </form>
