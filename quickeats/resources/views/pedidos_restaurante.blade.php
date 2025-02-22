440787adfbc0395318919f9b3577c1cf9777b609
lais <laismtelles@gmail.com>
2025-02-22 15:05:03 -0300

    Adição funcionalidade aceitar/ recusar cancelamento

--- quickeats/resources/views/pedidos_restaurante.blade.php
+++ quickeats/resources/views/pedidos_restaurante.blade.php
@@ -16,6 +16,11 @@
             </button>
         </li>
         <li class="nav-item" role="presentation">
+            <button class="nav-link" id="aguardando-cancelamento-tab" data-bs-toggle="tab" data-bs-target="#aguardando-cancelamento" type="button" role="tab">
+                Aguardando Cancelamento
+            </button>
+        </li>
+        <li class="nav-item" role="presentation">
             <button class="nav-link" id="rota-tab" data-bs-toggle="tab" data-bs-target="#rota" type="button" role="tab">
                 Em Rota de Entrega
             </button>
@@ -25,10 +30,20 @@
                 Finalizados
             </button>
         </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="cancelado-tab" data-bs-toggle="tab" data-bs-target="#cancelado" type="button" role="tab">
+                Cancelados
+            </button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="recusado-tab" data-bs-toggle="tab" data-bs-target="#recusado" type="button" role="tab">
+                Recusados
+            </button>
+        </li>
     </ul>
 
     <div class="tab-content mt-3" id="pedidosTabsContent">
-    @foreach(['pendentes' => 2, 'preparacao' => 3, 'rota' => 4, 'finalizados' => 5] as $tab => $status)
+    @foreach(['pendentes' => 2, 'preparacao' => 3, 'rota' => 4, 'finalizados' => 5, 'aguardando-cancelamento' => 6, 'cancelado' => 7, 'recusado' => 8] as $tab => $status)
         <div class="tab-pane fade @if($loop->first) show active @endif" id="{{ $tab }}" role="tabpanel">
             <div class="row">
                 @php
@@ -59,19 +74,37 @@
                                     <strong>Produtos:</strong> {{ $pedido->produtos }}
                                 </p>
                                 @if($pedido->status_entrega == 2) 
-                                    {{-- Se o pedido estiver aguardando aprovação --}}
-                                    <form action="{{ route('pedidos_status', $pedido->id_pedido) }}" method="POST">
-                                        @csrf
-                                        <input type="hidden" name="novo_status" value="3">
-                                        <button type="submit" class="btn btn-success">Aceitar Pedido</button>
-                                    </form>
+                                    <div class="d-flex gap-2">
+                                        <form action="{{ route('pedidos_status', $pedido->id_pedido) }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="novo_status" value="3">
+                                            <button type="submit" class="btn btn-success">Aceitar Pedido</button>
+                                        </form>
+                                        <form action="{{ route('pedidos_status', $pedido->id_pedido) }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="novo_status" value="8">
+                                            <button type="submit" class="btn btn-danger">Recusar Pedido</button>
+                                        </form>
+                                    </div>
                                 @elseif($pedido->status_entrega == 3) 
-                                    {{-- Se o pedido estiver em preparação --}}
                                     <form action="{{ route('pedidos_status', $pedido->id_pedido) }}" method="POST">
                                         @csrf
                                         <input type="hidden" name="novo_status" value="4">
                                         <button type="submit" class="btn btn-primary">Marcar como Pronto</button>
                                     </form>
+                                @elseif($pedido->status_entrega == 6) 
+                                    <div class="d-flex gap-2">
+                                        <form action="{{ route('pedidos_status', $pedido->id_pedido) }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="novo_status" value="7">
+                                            <button type="submit" class="btn btn-danger">Aprovar Cancelamento</button>
+                                        </form>
+                                        <form action="{{ route('pedidos_status', $pedido->id_pedido) }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="novo_status" value="3">
+                                            <button type="submit" class="btn btn-warning">Recusar Cancelamento</button>
+                                        </form>
+                                    </div>
                                 @endif
                             </div>
                         </div>
@@ -83,7 +116,7 @@
             @endif
         </div>
     @endforeach
-</div>
+    </div>
 
 </section>
 @endsection
