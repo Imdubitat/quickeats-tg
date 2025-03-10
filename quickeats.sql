9b638c647ecaa9b836a4c19cec8274bab1efd510
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2025-03-10 14:15:24 -0300

    Adição do campo "email_verificado" na tabela "estabelecimentos"

--- quickeats.sql
+++ quickeats.sql
@@ -542,7 +542,8 @@ CREATE TABLE `estabelecimentos` (
   `inicio_expediente` time NOT NULL,
   `termino_expediente` time NOT NULL,
   `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
+  `senha` varchar(255) NOT NULL,
+  `email_verificado` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
