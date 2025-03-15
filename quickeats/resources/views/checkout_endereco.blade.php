fdab36b82c84f5b41bba66f45ce884962149d607
lais <laismtelles@gmail.com>
2025-03-15 15:47:42 -0300

    Ajuste de layout páginas de checkout do pedido

--- quickeats/resources/views/checkout_endereco.blade.php
+++ quickeats/resources/views/checkout_endereco.blade.php
@@ -10,20 +10,17 @@
     <div class="container">
         <form action="{{ route('exibir_pagamentos') }}" method="POST">
             @csrf
-            @php $i = 0; @endphp
             @foreach($enderecos as $e)
             <div class="row border p-4 rounded-4 align-items-center mb-3">
                 <div class="col-md-1 d-flex align-items-center">
                     <input class="form-check-input" type="radio" name="endereco" id="endereco{{ $e->id_endereco }}" value="{{ $e->id_endereco }}" required>
                 </div>
                 <label class="col-md-11" for="endereco{{ $e->id_endereco }}">
-                    <h5>Endereço {{ $i + 1 }}</h5>
-                    <h6>Logradouro: {{ $e->logradouro }}, {{ $e->numero }}</h6>
-                    <h6>Bairro: {{ $e->bairro }}, {{ $e->cep }}</h6>
-                    <h6>Cidade: {{ $e->cidade }} - {{ $e->estado }}</h6>
+                    <h5>{{ $e->logradouro }}, {{ $e->numero }}</h5>
+                    <h6>{{ $e->bairro }}, {{ $e->cidade }} - {{ $e->estado }}</h6>
+                    <h6>CEP: {{ $e->cep }}</h6>
                 </label>
             </div>
-            @php $i++; @endphp
             @endforeach
 
             <div class="text-center mt-4">
--- quickeats/resources/views/checkout_pagamento.blade.php
+++ quickeats/resources/views/checkout_pagamento.blade.php
@@ -10,18 +10,15 @@
     <div class="container">
         <form action="{{ route('realizar_pedido') }}" method="POST">
             @csrf
-            @php $i = 0; @endphp
             @foreach($formas_pagamento as $fp)
             <div class="row border p-4 rounded-4 align-items-center mb-3">
                 <div class="col-md-1 d-flex align-items-center">
                     <input class="form-check-input" type="radio" name="pagamento" id="pagamento{{ $fp->id_formapag }}" value="{{ $fp->id_formapag }}" required>
                 </div>
                 <label class="col-md-11" for="pagamento{{ $fp->id_formapag }}">
-                    <h5>Forma de pagamento {{ $i + 1 }}</h5>
-                    <h6>Descrição: {{ $fp->descricao }}</h6>
+                    <h5>{{ $fp->descricao }}</h6>
                 </label>
             </div>
-            @php $i++; @endphp
             @endforeach
 
             <div class="text-center mt-4">
